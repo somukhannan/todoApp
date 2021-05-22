e72d97c6917fa125eae6e238b5d0c8ad5357e23e
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-05-22 11:52:37 +0530

    Completed services for Toggle Todo.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,16 @@
 import { React } from 'react';
+import context from '../core/context';
 
 const Todo = (todo) => {
-	const { text, id } = todo;
+	const { text, id, completed } = todo;
 
 	return <div key={ id }>
 		<span>
-			<input type="checkbox"/>
+			<input
+				type="checkbox"
+				defaultChecked={ completed }
+				onChange={ () => context.actions.toggleTodo(todo) }
+			/>
 		</span>
 		<span>
 			{ text }
--- src/core/actions.js
+++ src/core/actions.js
@@ -1,21 +1,25 @@
 import context from '../core/context';
+import { rndString } from '@laufire/utils/random';
+import TargetManager from '../services/todoManager';
+const randomStringLen = 8;
 
 const addToDo = ({ state }) => ({
-	todos: state.todos.concat({ text: context.state.input, id: Date.now() }),
+	todos: state.todos.concat({ text: context.state.input,
+		id: rndString(randomStringLen), completed: false }),
 });
 
 const setInput = (dummy, input) => ({
 	input,
 });
 
-const increaseCount = ({ state }) => ({
-	count: state.count + 1,
+const toggleTodo = ({ state }, todo) => ({
+	todos: TargetManager.toggleTodo(state.todos, todo),
 });
 
 const actions = {
-	increaseCount,
 	setInput,
 	addToDo,
+	toggleTodo,
 };
 
 export default actions;
