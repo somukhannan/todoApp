da8e8e1fecac3824a95e2fba49722b09ae962fff
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-05-22 10:10:06 +0530

    UI Checkbox done.

--- src/components/simpleButton.js
+++ src/components/simpleButton.js
@@ -2,7 +2,7 @@ import { React } from 'react';
 import context from '../core/context';
 
 const SimpleButton = () =>
-	<button onClick={ () => context.actions.addToDo(context.state.input) }>
+	<button onClick={ () => context.actions.addToDo() }>
 		Add</button>;
 
 export default SimpleButton;
