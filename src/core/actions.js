dbe55cec4d8a9abe9ccd620ace943671f2a75e72
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-05-23 13:03:34 +0530

    Pre step for feature 4 i.e. ToggleAll.

--- src/core/actions.js
+++ src/core/actions.js
@@ -16,11 +16,16 @@ const toDoRemoval = ({ state }, todo) => ({
 	todos: TargetManager.toDoRemoval(state.todos, todo),
 });
 
+const toggleAllTodos = ({ state }, ischecked) => ({
+	todos: TargetManager.toggleAllTodos(state.todos, ischecked),
+});
+
 const actions = {
 	setInput,
 	addToDo,
 	toggleTodo,
 	toDoRemoval,
+	toggleAllTodos,
 };
 
 export default actions;
