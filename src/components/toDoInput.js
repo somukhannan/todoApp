9401cb04f269911677bd737695f6f432c6c09813
Somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-04 19:23:22 +0530

    Actions keys added.

--- src/components/toDoInput.js
+++ src/components/toDoInput.js
@@ -1,10 +1,16 @@
 import { React } from 'react';
 import context from '../core/context';
 
+const actionKeys = {
+	Enter: () => context.actions.addTodo(),
+	Escape: () => context.actions.setInput(''),
+};
+
 const todoInput = () =>
 	<input
 		value={ context.state.input }
 		onChange={ (evt) => context.actions.setInput(evt.target.value) }
+		onKeyUp={ (evt) => actionKeys[evt.code] && actionKeys[evt.code]() }
 	/>;
 
 export default todoInput;
