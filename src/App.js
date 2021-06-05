670cbe9a332711089d354a2e439d06cab2f01d00
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-06-05 11:10:14 +0530

    removed refresh string id.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { React, useEffect } from 'react';
 import './App.css';
-import context from './core/context';
 import SampleService from './services/sample';
 import todoInput from './components/todoInput.js';
 import todoList from './components/todoList.js';
@@ -11,8 +10,7 @@ import actionButton from './components/actionButton.js';
 
 const App = () => {
 	useEffect(SampleService.sayHai, []);
-	// eslint-disable-next-line no-console
-	console.log(context.state);
+
 	return (
 		<div className="App">
 			<div>
--- src/core/config.js
+++ src/core/config.js
@@ -1,5 +1,5 @@
 const config = {
-	countStart: 0,
+	idLength: 16,
 };
 
 export default config;
