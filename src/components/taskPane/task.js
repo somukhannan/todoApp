82dbcc7b9f58b3d6a2593ffb205ef7993d34ddf4
Somukhannan Thiyagarajan <somukhannan8@gmail.com>
2021-06-09 14:17:57 +0000

    Added addButton to task.

--- src/components/taskPane/task.js
+++ src/components/taskPane/task.js
@@ -4,10 +4,18 @@ import { React } from 'react';
 const removeButton = (task) =>
 	<button onClick={ () => context.actions.removeTask(task) }>X</button>;
 
+const addButton = (task) =>
+	<button onClick={ () => {
+		context.actions.addTaskTodo(task) ;
+		context.actions.removeTask(task);
+	} }
+	>+</button>;
+
 const Task = (task) => {
 	const { id, text } = task;
 
 	return <div key={ id }>
+		<span>{addButton(task)}</span>
 		<span>{text}</span>
 		<span>{removeButton(task)}</span>
 	</div>;
