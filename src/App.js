f9a17a8e117df33f52d28263a3b992300c74d040
somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-10 14:28:58 +0000

    Added ticket in service.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import TodoPane from './components/todoPane';
 import TaskManager from './services/taskManager.js';
 import TaskPane from './components/taskPane';
+import Ticker from './services/ticker.js';
 
 const App = () => {
 	useEffect(TaskManager.init, []);
+	useEffect(Ticker.start, []);
 
 	return (
 		<div className="App">
